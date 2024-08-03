ff61c7bf661108d7b61323210e13bb04ced6ab7b
John <john.msme1@gmail.com>
2024-08-03 18:08:01 +0100

    about hero section

--- src/app/about-coalesy/page.js
+++ src/app/about-coalesy/page.js
@@ -1,3 +1,4 @@
+import Hero from '@/components/about-coalesy/hero/Hero'
 import Navbar from '@/components/navbar/Navbar'
 import React from 'react'
 
@@ -5,7 +6,7 @@ const page = () => {
   return (
     <div>
         <Navbar/>
-        
+        <Hero/>
     </div>
   )
 }
--- src/components/about-coalesy/hero/Hero.jsx
+++ src/components/about-coalesy/hero/Hero.jsx
@@ -3,27 +3,26 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="lg:py-[30px] py-[20px] ">
+    <div className="lg:py-[100px] py-[20px] ">
       <div className=" bg-[#10091B] lg:pl-[140px] pl-0 lg:flex lg:flex-row flex-col items-center lg:justify-between justify-center">
         <div className="">
           <h1 className="text-[40px] lg:w-[442px] font-bold md:w-[400px] w-[350px] lg:leading-[44px] leading-[33px]">
-            Powerful Web3 Ticketing APIs
+          We&apos;re building the future of connection
           </h1>
           <p className="w-[442px] opacity-70 pt-6 pb-14">
-            Share your creations, collaborate with other innovators, and push
-            the boundaries of event technology together.
+          The world of events is evolving, and Coalesy is here to guide you on this exciting journey. We are a revolutionary Web3 ticketing platform that unlocks a universe of possibilities for individuals, businesses and developers .
           </p>
           <button className="bg-[#FF5B96] text-white py-[8px] px-[24px] rounded-md w-[205px] cursor-pointer">
-            Read docs
+            Read whitepaper
           </button>
         </div>
         <div className="justify-end items-end flex">
           <Image
             className=""
-            src="./developers-hero.svg"
+            src="./about-coalesy-hero.svg"
             alt="Next.js Logo"
-            width={500}
-            height={500}
+            width={805}
+            height={900}
           />
         </div>
       </div>
